cec7786dbeac5e5bef80b1e1d9b38b03a613496f
Ruslan Zamoshnikov <r5718293456@gmail.com>
2024-06-20 01:29:36 +0300

    Created the chapter-1 folder and the file main.cpp

---
+++ chapter-1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int first_number, second_number;
+
+	cout << "Введите первое число: "; cin >> first_number;
+	cout << "Введите второеое число: "; cin >> second_number << endl;
+
+	cout << "Сумма этих чисел: " << first_number + second_number << endl;
+	cout << "Разность этих чисел: " << first_number - second_number;
+
+	return 0;
+}
